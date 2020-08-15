ccb182d9427614c5ca4ebdcdcafbbaea0a6a5597
Jess <jess.marie.sullivan@gmail.com>
2020-08-15 11:50:31 +0100

    adds instance variables for name and age

--- animals.rb
+++ animals.rb
@@ -1,27 +1,54 @@
 class Bird
-    def talk    
-        puts "Chirp! Chirp!"
+    def make_up_name
+        @name = "Tweetie"
+    end
+    def talk 
+        puts "#{@name} says Chirp! Chirp!"
     end
     def move(destination)
-        puts "Flying to #{destination}."
+        puts "#{@name} flies to #{destination}."
+    end
+    def make_up_age
+        @age = 3
+    end
+    def report_age
+        puts "#{@name} is #{@age} years old."
     end
 end
 
-class Dog
+class Dog 
+    def make_up_name
+        @name = "Cody"
+    end
     def talk
-        puts "Bark!"
+        puts "#{@name} says Bark!"
     end
     def move(destination)
-        puts "Running to the #{destination}."
+        puts "#{@name} runs to the #{destination}."
+    end
+    def make_up_age
+        @age = 5
+    end
+    def report_age
+        puts "#{@name} is #{@age} years old."
     end
 end
 
 class Cat
+    def make_up_name
+        @name = "Martha"
+    end
     def talk
-        puts "Meow."
+        puts "#{@name} says Meow."
     end
     def move(destination)
-        puts "Meanders to the #{destination}."
+        puts "#{@name} meanders to the #{destination}."
+    end
+    def make_up_age
+        @age = 14
+    end
+    def report_age
+        puts "#{@name} is #{@age} years old."
     end
 end
 
@@ -29,7 +56,9 @@ bird = Bird.new
 dog = Dog.new
 cat = Cat.new
 
-bird.move("tree")
-dog.talk
-cat.move("window")
+puts cat.make_up_name
+puts cat.talk
+puts cat.move("house")
+puts cat.make_up_age
+puts cat.report_age
 

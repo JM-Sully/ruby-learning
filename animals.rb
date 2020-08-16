0b0747cbdf01e7c77511ea99f5a4c5eb543f6c9d
Jess <jess.marie.sullivan@gmail.com>
2020-08-16 10:03:37 +0100

    addes accessor methods to dog class

--- animals.rb
+++ animals.rb
@@ -1,64 +1,90 @@
 class Bird
+
     def make_up_name
         @name = "Tweetie"
     end
+
     def talk 
         puts "#{@name} says Chirp! Chirp!"
     end
+
     def move(destination)
         puts "#{@name} flies to #{destination}."
     end
+
     def make_up_age
         @age = 3
     end
+
     def report_age
         puts "#{@name} is #{@age} years old."
     end
+    
 end
 
 class Dog 
-    def make_up_name
-        @name = "Cody"
+    def name=(new_value)
+        @name = new_value
     end
+
+    def name
+        @name
+    end
+
+    def age=(new_value)
+        @age = new_value
+    end
+
+    def age
+        @age
+    end
+
+    def report_age
+        puts "#{@name} is #{@age} years old."
+    end
+
     def talk
         puts "#{@name} says Bark!"
     end
+
     def move(destination)
         puts "#{@name} runs to the #{destination}."
     end
-    def make_up_age
-        @age = 5
-    end
-    def report_age
-        puts "#{@name} is #{@age} years old."
-    end
+
 end
 
 class Cat
+
     def make_up_name
         @name = "Martha"
     end
+
     def talk
         puts "#{@name} says Meow."
     end
+
     def move(destination)
         puts "#{@name} meanders to the #{destination}."
     end
+
     def make_up_age
         @age = 14
     end
+
     def report_age
         puts "#{@name} is #{@age} years old."
     end
+
 end
 
-bird = Bird.new
-dog = Dog.new
-cat = Cat.new
+cody = Dog.new
+cody.name = "Cody"
+cody.age = 7
+benji = Dog.new
+benji.name = "Benji"
+benji.age = 16
 
-puts cat.make_up_name
-puts cat.talk
-puts cat.move("house")
-puts cat.make_up_age
-puts cat.report_age
+puts cody.report_age
+puts benji.report_age
+
 

a2acdea44a7b697ed38b0685fb09a02122050a15
Jess <jess.marie.sullivan@gmail.com>
2020-08-17 09:15:10 +0100

    attribute accessor activity

--- animals.rb
+++ animals.rb
@@ -23,21 +23,8 @@ class Bird
 end
 
 class Dog 
-    def name=(new_value)
-        @name = new_value
-    end
 
-    def name
-        @name
-    end
-
-    def age=(new_value)
-        @age = new_value
-    end
-
-    def age
-        @age
-    end
+    attr_accessor :name :age
 
     def report_age
         puts "#{@name} is #{@age} years old."

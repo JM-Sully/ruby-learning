bf615737534cad844b6172af59349e8103d39b02
Jess <jess.marie.sullivan@gmail.com>
2020-08-29 15:43:19 +0100

    creates a new hash using {}

--- votes.rb
+++ votes.rb
@@ -3,4 +3,15 @@ File.open("votes.txt") do |read_votes|
     lines = read_votes.readlines
 end
 
-p lines
+votes = {} #creates an empty hash
+
+lines.each do |line|
+    name = line.chomp
+    if votes[name] #don't need to add '!= nil' If the vote name exists, add 1
+        votes[name] += 1
+    else #if the vote name doesn't exist, at the vote name as a key and assign it a value of one
+        votes[name] = 1
+    end
+end
+
+p votes

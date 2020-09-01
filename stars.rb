3bc4fdeea3b6f62880aeb4ec0decf16132346c5d
Jess <jess.marie.sullivan@gmail.com>
2020-09-01 08:23:10 +0100

    uses a hash default block to organize planets

--- stars.rb
+++ stars.rb
@@ -2,17 +2,15 @@ class CelestialBody
     attr_accessor :type, :name
 end
 
-altair = CelestialBody.new
-altair.name = 'Altair'
-altair.type = 'star'
-polaris = CelestialBody.new
-polaris.name = 'Polaris'
-polaris.type = 'star'
-vega = CelestialBody.new
-vega.name = 'Vega'
-vega.type = 'star'
+bodies = Hash.new do |hash, key|
+    body = CelestialBody.new
+    body.type = "planet"
+    hash[key] = body
+end
 
-puts altair.name, polaris.name, vega.name
-puts altair.object_id
-puts polaris.object_id
-puts vega.object_id
+bodies['Mars'].name = 'Mars'
+bodies['Europa'].name = 'Europa'
+bodies['Europa'].type = 'moon'
+bodies['Venus'].name = 'Venus'
+
+p bodies

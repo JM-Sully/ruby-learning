5bfd20ffb04aefd63fdcc037816f52b291acd198
Jess <jess.marie.sullivan@gmail.com>
2020-09-04 08:20:03 +0100

    adds comparable module

--- steak.rb
+++ steak.rb
@@ -1,19 +1,33 @@
 class Steak
 
+    include Comparable
+
     GRADE_SCORES = {"Prime" => 3, "Choice" => 2, "Select" => 1} #now defined as a constant
     attr_accessor :grade
 
-    def >(other)
-        GRADE_SCORES[grade] > GRADE_SCORES[other.grade]
+    def <=>(other)
+        if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
+            return -1
+        elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
+            return 0
+        else
+            return 1
+        end
     end
 
 end
 
-first_steak = Steak.new
-first_steak.grade = "Prime"
-second_steak = Steak.new
-second_steak.grade = "Choice"
+prime = Steak.new
+prime.grade = "Prime"
+choice = Steak.new
+choice.grade = "Choice"
+select = Steak.new
+select.grade = "Select"
 
-if first_steak > second_steak
-    puts "I'll take #{first_steak.inspect}"
-end
+puts "prime > choice: #{prime > choice}"
+puts "prime < select #{prime < select}"
+puts "select == select #{select == select}"
+puts "select <= select #{select <= select}"
+puts "select >= select #{select >= select}"
+print "choice.between?(select, prime): "
+puts choice.between?(select, prime)

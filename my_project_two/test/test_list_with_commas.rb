31ec591e6990ca0e530212d29c0f1c6d1527ab43
Jess <jess.marie.sullivan@gmail.com>
2020-09-12 08:35:32 +0100

    replaces assert with assert_equal

--- my_project_two/test/test_list_with_commas.rb
+++ my_project_two/test/test_list_with_commas.rb
@@ -6,19 +6,19 @@ class TestListWithCommas < Minitest::Test
     def test_it_prints_one_word_alone
         list = ListWithCommas.new
         list.items = ['Cody']
-        assert('Cody' == list.join, "Return value didn't equal 'Cody'")
+        assert_equal('Cody', list.join)
     end
     
     def test_it_joins_two_words_with_and
         list = ListWithCommas.new
         list.items = ['Cody', 'Jess']
-        assert('Cody and Jess' == list.join)
+        assert_equal('Cody and Jess', list.join)
     end
 
     def test_it_joins_three_words_with_commas
         list = ListWithCommas.new
         list.items = ['Cody', 'Ady', 'Jess']
-        assert('Cody, Ady, and Jess' == list.join)
+        assert_equal('Cody, Ady, and Jess', list.join)
     end
 
 end

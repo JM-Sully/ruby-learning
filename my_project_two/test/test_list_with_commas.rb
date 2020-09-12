b26ce9d79ff782a1c1e53d35fe560d31768e1980
Jess <jess.marie.sullivan@gmail.com>
2020-09-12 08:31:25 +0100

    adds a failure message to list of one item

--- my_project_two/test/test_list_with_commas.rb
+++ my_project_two/test/test_list_with_commas.rb
@@ -2,17 +2,23 @@ require 'minitest/autorun'
 require 'list_with_commas'
 
 class TestListWithCommas < Minitest::Test
+
+    def test_it_prints_one_word_alone
+        list = ListWithCommas.new
+        list.items = ['Cody']
+        assert('Cody' == list.join, "Return value didn't equal 'Cody'")
+    end
     
     def test_it_joins_two_words_with_and
         list = ListWithCommas.new
         list.items = ['Cody', 'Jess']
-        assert('Cody and Jess.' == list.join)
+        assert('Cody and Jess' == list.join)
     end
 
     def test_it_joins_three_words_with_commas
         list = ListWithCommas.new
         list.items = ['Cody', 'Ady', 'Jess']
-        assert('Cody, Ady, and Jess.' == list.join)
+        assert('Cody, Ady, and Jess' == list.join)
     end
 
 end

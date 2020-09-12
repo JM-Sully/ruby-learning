d7b81dcd60ccf1e287f90e2839c83017b13bdf94
Jess <jess.marie.sullivan@gmail.com>
2020-09-12 08:57:29 +0100

    adds setup method to test to create a new list for each test

--- my_project_two/test/test_list_with_commas.rb
+++ my_project_two/test/test_list_with_commas.rb
@@ -3,22 +3,23 @@ require 'list_with_commas'
 
 class TestListWithCommas < Minitest::Test
 
+    def setup
+        @list = ListWithCommas.new
+    end
+
     def test_it_prints_one_word_alone
-        list = ListWithCommas.new
-        list.items = ['Cody']
-        assert_equal('Cody', list.join)
+        @list.items = ['Cody'] #change list to @list to use instance variable
+        assert_equal('Cody', @list.join) #change list to @list to use instance variable
     end
     
     def test_it_joins_two_words_with_and
-        list = ListWithCommas.new
-        list.items = ['Cody', 'Jess']
-        assert_equal('Cody and Jess', list.join)
+        @list.items = ['Cody', 'Jess']
+        assert_equal('Cody and Jess', @list.join)
     end
 
     def test_it_joins_three_words_with_commas
-        list = ListWithCommas.new
-        list.items = ['Cody', 'Ady', 'Jess']
-        assert_equal('Cody, Ady, and Jess', list.join)
+        @list.items = ['Cody', 'Ady', 'Jess']
+        assert_equal('Cody, Ady, and Jess', @list.join)
     end
 
 end
